feb61a9cb407d8929233a8e1265cdca71733d934
Jessica-Muk <127817628+Jessica-Muk@users.noreply.github.com>
2024-04-05 12:32:39 +0300

    22th commits/added code for signinbygoogle + logoutbygoogle

--- lib/friendlyDashboard.dart
+++ lib/friendlyDashboard.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:speech_aid/CustomAppBar.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 import 'package:speech_aid/alphabetic.dart';
-
 
 class FriendlyDashboard extends StatefulWidget {
   const FriendlyDashboard({Key? key}) : super(key: key);
@@ -12,22 +12,16 @@ class FriendlyDashboard extends StatefulWidget {
 
 class _FriendlyDashboardState extends State<FriendlyDashboard> {
   int notificationCount = 0; // Initialize notification count
+  final GoogleSignIn _googleSignIn = GoogleSignIn();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: CustomAppBar(
+      appBar: AppBar(
         title: const Text('الصفحة الرئيسية'),
-        notificationCount: notificationCount,
-        onMenuItemSelected: (String value) {
-          // Handle menu item selection here
-          if (value == 'Notification') {
-            // Handle notification option
-            setState(() {
-              notificationCount = 0; // Reset notification count
-            });
-          }
-        }, 
+        actions: [
+          _buildPopupMenuButton(),
+        ],
       ),
       body: Center(
         child: Padding(
@@ -119,5 +113,79 @@ class _FriendlyDashboardState extends State<FriendlyDashboard> {
         ),
       ),
     );
+  }
+
+  Widget _buildPopupMenuButton() {
+    bool hasNotification = notificationCount > 0;
+
+    return PopupMenuButton(
+      icon: CircleAvatar(
+        backgroundImage: AssetImage(
+          'assets/${hasNotification ? 'logo_notification.png' : 'logo.png'}',
+        ),
+      ),
+      itemBuilder: (BuildContext context) {
+        return <PopupMenuEntry>[
+          _buildPopupMenuItem('Profile', Icons.person, 'الحساب الشخصي'),
+          _buildPopupMenuItem('Notification', Icons.notifications, 'الإشعارات',
+              hasNotification ? Colors.red : Colors.black),
+          _buildPopupMenuItem('Recordings', Icons.mic, 'صفحة التسجيلات'),
+          _buildPopupMenuItem('History', Icons.history, 'تاريخ التسجيلات'),
+          _buildPopupMenuItem(
+              'Therapist Hospital', Icons.local_hospital, 'موقع العيادة'),
+          _buildPopupMenuItem('Therapist Info', Icons.info, 'معلومات المعالج'),
+          _buildPopupMenuItem('Log Out', Icons.logout, 'تسجيل الخروج'),
+        ];
+      },
+      onSelected: (value) {
+        if (value == 'Log Out') {
+          _signOut();
+        } else if (value == 'Notification') {
+          setState(() {
+            notificationCount = 0; // Reset notification count
+          });
+        } else {
+          // Handle other menu items
+        }
+      },
+    );
+  }
+
+  PopupMenuItem _buildPopupMenuItem(String value, IconData icon, String title,
+      [Color? textColor]) {
+    return PopupMenuItem(
+      value: value,
+      height: 40,
+      child: Row(
+        children: [
+          Icon(icon, color: textColor ?? Colors.black, size: 24),
+          const SizedBox(width: 5),
+          Text(
+            title,
+            style: TextStyle(
+              fontSize: 16,
+              color: textColor ?? Colors.black,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  void _signOut() async {
+    try {
+      // Sign out from Google
+      await _googleSignIn.signOut();
+
+      // Sign out from Firebase
+      await FirebaseAuth.instance.signOut();
+
+      // Navigate to login screen and clear route history
+      Navigator.pushNamedAndRemoveUntil(context, 'login', (route) => false);
+
+      print('User signed out successfully.');
+    } catch (error) {
+      print('Error signing out: $error');
+    }
   }
 }
